69cf94541f86472def01325345c4dd313606573a
JB-05 <jbmachan2005@gmail.com>
2024-03-10 00:20:30 +0530

    commit after adding modifications

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const articleRouter = require("./routes/article")
 const mongoose = require('mongoose')
 const app = express()
 
+
+app.set('view engine','ejs')
+
 app.get('/',(req,res)=>{
-    res.send("Welcome to my new project")
+    res.render('index');
 })
+
+app.use('./articles' , articleRouter)
 
 app.listen(3000)
